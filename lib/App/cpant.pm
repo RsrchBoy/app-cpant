168c11f1142809d48d7e161050f4c69b4c8f27da
Chris Weyl <cweyl@alumni.drew.edu>
2014-12-27 21:40:26 -0800

    initial commit

---
+++ lib/App/cpant.pm
@@ -0,0 +1,19 @@
+package App::cpant;
+
+# ABSTRACT: The great new App::cpant!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
